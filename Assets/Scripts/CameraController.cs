0b48c838b5169af24b0e828cc08763335d59725a
fsmccrea <fsmccrea@gmail.com>
2018-09-13 09:02:57 -0400

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public LayerMask obstacles;
+	 GameObject cameraFocus;
+	 GameObject camRayTarget;
+	 Camera theCamera;
+
+	public Vector2 sensitivity = new Vector2(3, 3);
+	public float smoothing = 3;
+	public float followDist = 3;
+	public float camMaxDistance = 7;
+	[Range(0.0f, 1.0f)]
+	public float obstacleDistBuffer = 1f;
+
+	Vector3 _rayTargetPosition;
+	bool _mouseLock = true;
+	
+	Vector2 _mouseDelta;
+	Vector2 _smoothMouse;
+	Vector2 _mouseAbsolute;
+	Vector3 _currentVelocity;
+
+	// Use this for initialization
+	void Start () {
+
+		cameraFocus = transform.GetChild(0).gameObject;
+		camRayTarget = cameraFocus.transform.GetChild(0).gameObject;
+		theCamera = cameraFocus.transform.GetChild(1).GetComponent<Camera>();
+
+		_rayTargetPosition = new Vector3 (0, 0, -camMaxDistance);
+		camRayTarget.transform.localPosition = _rayTargetPosition;
+
+		Cursor.lockState = CursorLockMode.Locked;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		//mouse delta per frame
+		_mouseDelta = new Vector2 (Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		CheckCursorLock();
+		MoveCamera();
+		CameraAvoidObstacles();
+	}
+	
+	void CheckCursorLock() {
+		if (Input.GetButtonDown("Pause"))
+			_mouseLock = !_mouseLock;
+
+		if (_mouseLock) {
+			Cursor.lockState = CursorLockMode.Locked;
+		} else {
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+
+	void MoveCamera() {
+		//camera follow player
+		transform.position = Vector3.SmoothDamp(transform.position, player.transform.position, ref _currentVelocity, followDist * Time.deltaTime);
+
+		//camera angle
+		if (_mouseLock) {
+			_mouseDelta = Vector2.Scale (_mouseDelta, sensitivity);
+			_smoothMouse = Vector3.Lerp(_smoothMouse, _mouseDelta, 1f / smoothing);
+
+			_mouseAbsolute += _smoothMouse;
+			_mouseAbsolute.y = Mathf.Clamp(_mouseAbsolute.y, -60f, 90f);
+
+			transform.localRotation = Quaternion.AngleAxis(_mouseAbsolute.x, Vector3.up);
+			cameraFocus.transform.localRotation = Quaternion.AngleAxis(_mouseAbsolute.y, Vector3.right);
+		}
+	}
+
+	void CameraAvoidObstacles() {
+		_rayTargetPosition.z = -camMaxDistance - obstacleDistBuffer;
+		camRayTarget.transform.localPosition = _rayTargetPosition;
+
+		Vector3 dirToPoint = (camRayTarget.transform.position - cameraFocus.transform.position).normalized;
+
+		RaycastHit hit;
+		if (Physics.Linecast(cameraFocus.transform.position, camRayTarget.transform.position, out hit, obstacles)) {
+			Debug.DrawRay(hit.point, -dirToPoint, Color.blue);
+			theCamera.transform.position = hit.point - dirToPoint * obstacleDistBuffer;
+		} else {
+			Debug.DrawLine(cameraFocus.transform.position, camRayTarget.transform.position, Color.green);
+			theCamera.transform.localPosition = new Vector3 (0, 0, -camMaxDistance);
+		}
+	}
+
+	void OnApplicationFocus(bool hasFocus) {
+		_mouseLock = hasFocus;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float minWalkSpeed = 1;
+	public float maxWalkSpeed = 5;
+	public float runSpeed = 10;
+	public float turnSpeed = 8;
+	public float jumpHeight = 1;
+	public float accelTime = .1f;
+	[Range (0,1)]
+	public float airControl;
+
+	public GameObject playerCam;
+
+	CharacterController _controller;
+	Transform _groundChecker;
+	
+//	Vector2 inputMove;
+//	Vector2 _inputDir;
+	Vector3 inputMove;
+	Vector3 _inputDir;
+	bool _running;
+	bool _jumping;
+	bool _isGrounded;
+
+	float _gravity = -12f;
+
+	float _currentAngle;
+	float _currentTurnAccel;
+	float _currentSpeed;
+	float _currentAccel;
+
+	Vector3 _velocity;
+
+	float oldVel;
+
+	// Use this for initialization
+	void Start () {
+
+		_controller = GetComponent<CharacterController>();
+		_groundChecker = transform.GetChild(0);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+//		inputMove = new Vector2 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		inputMove = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		_inputDir = inputMove.normalized;
+		_running = Input.GetButton("Button5");
+
+		bool up = _velocity.y > 0;
+		_jumping = Input.GetButton("Button2");
+
+		_velocity.y += ((_jumping) ? _gravity : _gravity - 15) * Time.deltaTime;
+		if (_controller.isGrounded) _velocity.y = -.01f;
+
+		_jumping = Input.GetButton("Button2");
+		if (Input.GetButtonDown("Button2") && _controller.isGrounded)
+			Jump();
+
+		Move();
+	}
+
+	void Move() {
+
+		float cameraAngle = Mathf.Atan2 (playerCam.transform.forward.x, playerCam.transform.forward.z) * Mathf.Rad2Deg;
+		_inputDir = (Quaternion.Euler(0, cameraAngle, 0) * _inputDir);
+
+		//facing calc
+		float targetAngle = Mathf.Atan2 (_inputDir.x, _inputDir.z) * Mathf.Rad2Deg;// + cameraAngle;
+		_currentAngle = Mathf.LerpAngle (_currentAngle, targetAngle, Time.deltaTime * GetModifiedAccelTime(true, turnSpeed) * _inputDir.magnitude);
+
+		//thumbstick magnitude calc
+		float walkPercent = Mathf.Clamp(inputMove.magnitude, 0f, .8f) / 0.8f;
+		float currentWalkSpeed = Mathf.Lerp(minWalkSpeed, maxWalkSpeed, walkPercent);
+
+		//move calc
+		float targetSpeed = ((_running) ? runSpeed : currentWalkSpeed) * _inputDir.magnitude;
+		_currentSpeed = Mathf.SmoothDamp(_currentSpeed, targetSpeed, ref _currentAccel, GetModifiedAccelTime(false, accelTime));
+		if (_currentSpeed < .01f)
+		_currentSpeed = 0;
+//		_velocity = new Vector3(_inputDir.x, _velocity.y, _inputDir.y) * _currentSpeed * Time.deltaTime;
+		_velocity.x = _inputDir.x * _currentSpeed;
+		_velocity.z = _inputDir.z * _currentSpeed;
+		
+		//output
+		transform.eulerAngles = Vector3.up * _currentAngle;
+//		transform.Translate (Quaternion.Euler(0, cameraAngle, 0) * _velocity, Space.World);
+//		_controller.Move(Quaternion.Euler(0, cameraAngle, 0) * _velocity);
+		_controller.Move(_velocity * Time.deltaTime);
+	}
+
+	float GetModifiedAccelTime(bool rotation, float theAccelTime) {
+		if (rotation) {
+			if (_controller.isGrounded) {
+			return theAccelTime;
+			}
+			if (airControl == 0) {
+				return float.MinValue;
+			}
+			return theAccelTime * airControl;
+		}
+		if (_controller.isGrounded) {
+			return theAccelTime;
+		}
+		if (airControl == 0) {
+			return float.MaxValue;
+		}
+		return theAccelTime / airControl;
+	}
+
+	void Jump() {
+		_velocity.y = Mathf.Sqrt(-2 * jumpHeight * _gravity);
+	}
+}
