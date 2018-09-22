3a7c7484e228243fb62d6433a7f5a2dd5ae323af
fsmccrea <fsmccrea@gmail.com>
2018-09-22 00:50:23 -0400

    Update 7 (9/21/18) - movement
    -movement workin
    -gotta fix the hill grounded detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	//inspector variables
-	public float walkSpeed = 7;
+	public float maxWalkSpeed = 7;
+	public float minWalkSpeed = 1;
 
 	public float inputSmoothTime = .1f;
+
+	public float gravity = -35;
 
 	//object reference
 	public GameObject playerCam;
@@ -15,11 +18,13 @@ public class PlayerController : MonoBehaviour {
 //	Transform slopePeeper;
 
 	//model
-	Vector3 inputMove;
+//	Vector3 inputMove;
 	Vector3 smoothInput;
+	Vector3 velocity;
 	
 	//ref
 	Vector3 currentInputVelocity;
+	float currentGravAccel;
 	float oldFacingAngle;
 
 	void Start()
@@ -31,48 +36,78 @@ public class PlayerController : MonoBehaviour {
 	void Update()
 	{
 		//raw input vector from input axes
-		inputMove = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 inputMove = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
 		//call functions
-		Move();
-		PeepNormals();
+		GroundMove(inputMove);
 	}
 
-	void Move()
+	void GroundMove(Vector3 inputMove)
 	{
 		//camera angle relative to player
 		float cameraAngle = Mathf.Atan2 (playerCam.transform.forward.x, playerCam.transform.forward.z) * Mathf.Rad2Deg;
 		//input rotation so its relative to camera
 		inputMove = (Quaternion.Euler(0, cameraAngle, 0) * inputMove);
 
-		//smooth input (simple)
+		//smooth input (simple) -- r e p u r p o s e
 		smoothInput = Vector3.SmoothDamp(smoothInput, inputMove, ref currentInputVelocity, inputSmoothTime);
+
+		//lerp walk speed based on input magnitude
+		float walkSpeed = Mathf.Lerp (minWalkSpeed, maxWalkSpeed, Mathf.Clamp(inputMove.magnitude, 0f, 0.8f) / 0.8f);
 		
-		//facing
-		//if input magnitude is zero, set facing angle to last recorded facing angle, else set it to the angle of input
+		//facing -- add angle smoothing rather than doing it based on smoothInput
+		//if input magnitude is zero, set facing angle to last recorded facing angle, else set it to the angle of SMOOTHED input (looksnicer)
 		float facingAngle = ((inputMove.magnitude == 0) ? oldFacingAngle : Mathf.Atan2 (smoothInput.normalized.x, smoothInput.normalized.z) * Mathf.Rad2Deg);
+
+		//movement velocity (+gravity if not grounded)
+//		velocity = inputMove * walkSpeed + Vector3.up * gravity * Time.deltaTime;
+//		if (controller.isGrounded) {
+//			velocity = PeepNormals (inputMove * walkSpeed - Vector3.up * .1f);
+//		}
+		//if grounded, velocity is the inputmove*walkspeed (with small force to keep grounded) aligned with the normals of the ground
+		//if not grounded, velocity is the inputmove*walkspeed + grav accel on y axis
+//		velocity = ApplyGravity(inputMove, walkSpeed);
+//		velocity = PeepNormals (inputMove * walkSpeed);// - Vector3.up * 0.1f;
+		velocity = PeepNormals (inputMove) * walkSpeed;
+//		velocity.y = velocity.y - 1;
+//		velocity = inputMove * walkSpeed;
+//		velocity.y = velocity.y - .01f;
+		print (controller.isGrounded);
 
 		//old var
 		oldFacingAngle = facingAngle;
 
 		//MOVEMENT + ROTATION output
 //		controller.Move(inputMove.normalized * walkSpeed * Time.deltaTime);
-		controller.Move(PeepNormals() * walkSpeed * Time.deltaTime);
+		controller.Move(velocity * Time.deltaTime);
 		transform.eulerAngles = Vector3.up * facingAngle;
 	}
 
-	Vector3 PeepNormals()
+	Vector3 PeepNormals(Vector3 dir)
 	{
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out hit, 10, 1 << 9)) {
-			Debug.DrawLine (transform.position + Vector3.up, hit.point, Color.green);
-			Debug.DrawRay (hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal) * inputMove, Color.blue);
+	//		Debug.DrawLine (transform.position + Vector3.up, hit.point, Color.green);
+	//		Debug.DrawRay (hit.point, Quaternion.FromToRotation(Vector3.up, hit.normal) * inputMove, Color.blue);
 //			slopePeeper.rotation = Quaternion.FromToRotation (Vector3.up, hit.normal);
-			//temporary snap down to surface so it looks nice
+//			//temporary snap down to surface so it looks nice
 //			slopePeeper.position = hit.point;
-			return Quaternion.FromToRotation(Vector3.up, hit.normal) * inputMove;
+
+			//rotate input to normals below player
+			dir.y = dir.y - .1f;
+			return Quaternion.FromToRotation(Vector3.up, hit.normal) * dir.normalized;
 		}
-		return inputMove;
+		return dir.normalized;
+	}
+
+	Vector3 ApplyGravity (Vector3 dir, float speed) {
+//		velocity.y += gravity * Time.deltaTime;
+		if (controller.isGrounded) {
+			currentGravAccel = 0;
+			return PeepNormals (dir * speed + Vector3.up * .01f);
+		}
+		currentGravAccel += gravity + Time.deltaTime;
+		return dir * speed + Vector3.up * currentGravAccel;
 	}
 
 }
