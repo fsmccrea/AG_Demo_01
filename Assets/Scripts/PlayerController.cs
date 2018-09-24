65b6b1745979af6dd90de72e5ac38b1d1d426315
fsmccrea <fsmccrea@gmail.com>
2018-09-24 14:47:00 -0400

    Update 9 (9/24/18) - SLOPES
    -holy shit I fuckin made slopes work
    -double jump
    -head bonk stop works
    -new method of grounded detection
    -added a temporary scene reload button (select)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Temporary scene management
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
@@ -14,44 +16,68 @@ public class PlayerController : MonoBehaviour {
 
 	public float gravity = -35;
 
+	//temp
+	public float sphereCastRadius = .75f;
+	public float sphereCastDistance = .25f;
+
 	//object reference
 	public GameObject playerCam;
 	CharacterController controller;
+
+	//temp scene reference
+	Scene scene;
 
 	//model
 //	Vector3 inputMove;
 	Vector3 smoothInput;
 	Vector3 velocity;
 
+	bool isGrounded;
+//	Vector3[] groundCheckRays = new Vector3[8];
+
 	//tags
 	bool jumping;
 	
 	//ref
 	Vector3 currentInputVelocity;
+	Vector3 rotatedVector;
 	float facingAngle;
-//	float currentGravAccel;
+	float currentGravAccel;
 	float oldFacingAngle;
-//	bool canJump = true;
+	bool canJump;
 
 	void Start()
 	{
 		controller = GetComponent<CharacterController>();
+		scene = SceneManager.GetActiveScene();
 	}
 
 	void Update()
 	{
+		//temp scene management
+		if (Input.GetButtonDown("Cancel"))
+			SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
+
 		//raw input vector from input axes
 		Vector3 inputMove = new Vector3 (Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
 
+		//GROUND CHECK HERE TEMPORARILY
+//		isGrounded = GroundCheck();
+
 		//jumpin
 		jumping = Input.GetButton("Button2");
-		if (Input.GetButtonDown("Button2"))// && canJump)
+		if (Input.GetButtonDown("Button2") && canJump)
 			Jump();
 
 		ApplyGravity();
 
 		//call functions
 		Move(inputMove);
+	}
+
+	void FixedUpdate()
+	{
+		isGrounded = GroundCheck();
 	}
 
 	void Move(Vector3 inputMove)
@@ -75,26 +101,28 @@ public class PlayerController : MonoBehaviour {
 
 		//if grounded, velocity is the inputmove*walkspeed (with small force to keep grounded) aligned with the normals of the ground
 		//if not grounded, velocity is the inputmove*walkspeed + grav accel on y axis
-		if (controller.isGrounded) {
+		if (isGrounded) {
+			canJump = true;
 //			currentGravAccel = 0;
 //			canJump = true;
 	//		velocity = PeepGround (inputMove.normalized) * walkSpeed;
 	//		velocity.y = velocity.y > -.1f ? velocity.y = -.1f : velocity.y -= .1f;
 
 			//if grounded, give velocity ground movement
-			//y velocity handled by apply gravity
+			//y velocity handled by apply gravity --//NAH gotta get that smooth slope movement --////BUT WTF THOEEE IT HAS IT IN THERE
 			Vector3 targetVel = PeepGround (inputMove.normalized) * walkSpeed;
-			velocity = new Vector3 (targetVel.x, velocity.y, targetVel.z);
+			velocity = new Vector3 (targetVel.x, jumping ? velocity.y : targetVel.y, targetVel.z);
 
 		} else {
 //			velocity = ApplyGravity(inputMove * walkSpeed);
 			//if not grounded, do arial movement calculations
 			velocity.x = inputMove.x * walkSpeed;
 			velocity.z = inputMove.z * walkSpeed;
-			//y velocity handled by ApplyGravity
+			//y velocity handled by ApplyGravity -- but now its in here from there :O
+//			velocity.y = currentGravAccel;
 		//	velocity.y += gravity * Time.deltaTime;
 		}
-		print (transform.position.y);
+		print ("Grounded = " + isGrounded);
 	//	velocity = PeepGround (inputMove.normalized) * walkSpeed;
 
 		//old var
@@ -107,7 +135,7 @@ public class PlayerController : MonoBehaviour {
 
 	void Jump() {
 
-//		canJump = controller.isGrounded;
+		canJump = controller.isGrounded;
 		velocity.y = Mathf.Sqrt(-2 * jumpHeight * gravity);
 
 	}
@@ -117,7 +145,8 @@ public class PlayerController : MonoBehaviour {
 		RaycastHit hit;
 		if (Physics.Raycast(transform.position + Vector3.up, Vector3.down, out hit, 1.1f, 1 << 9)) {
 			//rotate input to normals below player
-			return Quaternion.FromToRotation(Vector3.up, hit.normal) * dir;
+			rotatedVector = Quaternion.FromToRotation(Vector3.up, hit.normal) * dir;
+			return rotatedVector;
 		}
 		return dir;
 	}
@@ -131,13 +160,44 @@ public class PlayerController : MonoBehaviour {
 
 	}
 	*/
-	void ApplyGravity () {
+	void ApplyGravity() {
 		float downward = velocity.y > 0 ? 0 : -10;
-		if (controller.isGrounded) {
-			velocity.y = jumping ? velocity.y : -.1f;
+	/*	if (isGrounded) {
+//			velocity.y = jumping ? velocity.y : -.1f;
+		//	if (jumping) {
+		//		velocity.y = velocity.y;
+		//	} else {
+		//		velocity.y = rotatedVector.y - 0.1f;
+		//	}
+			currentGravAccel = 0;
 		} else {
-			velocity.y += (gravity + downward) * Time.deltaTime;
+			currentGravAccel = velocity.y + (gravity + downward) * Time.deltaTime;
+		} */
+		currentGravAccel = isGrounded ? 0 : velocity.y + (gravity + downward) * Time.deltaTime;
+
+		if (!isGrounded)
+			velocity.y = currentGravAccel;
+
+		//for the head hit
+		if ((controller.collisionFlags & CollisionFlags.Above) != 0) {
+			if (velocity.y > 0)
+			velocity.y = 0;
 		}
 	}
+
+	bool GroundCheck() {
+	/*	foreach (Vector3 rayStart in groundCheckRays) {
+			Vector3 pos = new Vector3
+		}
+
+		//debugfornow
+		return false;
+		*/
+		RaycastHit hit;
+		if (Physics.SphereCast(transform.position + Vector3.up, sphereCastRadius, Vector3.down, out hit, sphereCastDistance, 1 << 9, QueryTriggerInteraction.Ignore))
+			return true;
+
+		return false;
+	} 
 
 }
